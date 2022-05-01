525c56f000ec8fd256a090d4d8223d9380c03338
SBDWolf <90091268+SBDWolf@users.noreply.github.com>
2022-05-01 05:06:45 +0200

    Add files via upload

--- src/newcode.asm
+++ src/newcode.asm
@@ -74,47 +74,139 @@ JMP {sg_trainer_code}
 check_pause_flag:
 LDA {pause_flag}
 CMP #$01
-BEQ draw_menu
+BEQ check_stage
 LDA #$00
 STA {current_cursor_position}
+STA {already_selected_sg_from_top_menu}
 JMP {done_offset}
 
+// check current stage, and store the corresponding scroll glitch in an area of memory.
+// the moment the scroll glitch variables get initialized, check for this and use different variables depending on the value of current_sg_to_practice
+// if equal to 0, go to a special case where you display that there's no sg to practice here.
+check_stage:
+CLV
+// if already loaded the scroll glitch to practice from the top menu, skip this check, otherwise, load a value corresponding to a top menu
+LDA {already_selected_sg_from_top_menu}
+CMP #$01
+BEQ check_menu_to_draw
 
+
+LDA {currstage}
+CMP #$0E
+BNE check_for_stage_13
+LDA {currsubstage}
+CMP #$01
+BNE check_for_stage_13
+LDA {simon_x_high_byte}
+CMP #$04
+BCS top_floor_drop
+// This value will indicate that there's multiple options for stage 14, advanced or simple
+LDA {14_simple_or_advanced}
+STA {current_sg_to_practice}
+BVC check_menu_to_draw
+
+top_floor_drop:
+// This value will indicate that there's multiple options for stage 14, top single or top multiple
+LDA {14_top_single_or_double}
+STA {current_sg_to_practice}
+BVC check_menu_to_draw
+check_for_stage_13:
+LDA {currstage}
+CMP #$0D
+BNE check_for_stage_06
+LDA {currsubstage}
+CMP #$00
+BNE check_for_stage_06
+LDA {13_sg}
+STA {current_sg_to_practice}
+BVC check_menu_to_draw
+check_for_stage_06:
+LDA {currstage}
+CMP #$06
+BNE no_sg
+LDA {currsubstage}
+CMP #$00
+BNE no_sg
+LDA {06_sg}
+STA {current_sg_to_practice}
+BVC check_menu_to_draw
+no_sg:
+
+
+
+
+
+
+
+
+
+
+check_menu_to_draw:
+
+LDA {current_sg_to_practice}
+CMP {14_top_single_or_double}
+BCC set_anything_else
+BEQ set_14_top_single_or_double_menu
+
+// 14_simple_or_advanced
+
+LDY #$00
+BVC draw_menu
+
+set_14_top_single_or_double_menu:
+
+// 14_top_single_or_double
+
+LDY #$0A
+BVC draw_menu
+
+set_anything_else:
+
+// Anything else
+LDY #$14
 
 draw_menu:
+LDA #$20
+STA {PPU_ADDR}
+LDA #$61
+STA {PPU_ADDR}
+LDA {menu_tables}, y
+STA {PPU_DATA}
+INY
+LDA {menu_tables}, y
+STA {PPU_DATA}
+INY
+LDA {menu_tables}, y
+STA {PPU_DATA}
+INY
+LDA {menu_tables}, y
+STA {PPU_DATA}
+INY
+LDA {menu_tables}, y
+STA {PPU_DATA}
+INY
 
 LDA #$20
 STA {PPU_ADDR}
-LDA #$60
+LDA #$81
 STA {PPU_ADDR}
-LDA #$00
+LDA {menu_tables}, y
 STA {PPU_DATA}
-LDA #$D2
+INY
+LDA {menu_tables}, y
 STA {PPU_DATA}
-LDA #$DD
+INY
+LDA {menu_tables}, y
 STA {PPU_DATA}
-LDA #$D1
+INY
+LDA {menu_tables}, y
 STA {PPU_DATA}
-LDA #$D4
+INY
+LDA {menu_tables}, y
 STA {PPU_DATA}
-LDA #$00
-STA {PPU_DATA}
+INY
 
-LDA #$20
-STA {PPU_ADDR}
-LDA #$80
-STA {PPU_ADDR}
-LDA #$00
-STA {PPU_DATA}
-LDA #$D4
-STA {PPU_DATA}
-LDA #$DD
-STA {PPU_DATA}
-LDA #$D1
-STA {PPU_DATA}
-LDA #$D2
-STA {PPU_DATA}
-
+check_cursor:
 // check for select input to change menu option
 LDA {currinput_oneframe}
 AND #$20
@@ -146,11 +238,83 @@ LDA #$DC
 STA {PPU_DATA}
 
 skip:
+LDA {current_sg_to_practice}
+CMP #$F0
+BCC on_mode_selection_menu
+LDA {currinput_oneframe}
+AND #$C0
+CMP #$80
+BEQ set_scroll_glitch
+CMP #$40
+BEQ set_scroll_glitch
+CMP #$C0
+BEQ set_scroll_glitch
+JMP {done_offset}
+
+set_scroll_glitch:
+LDA {current_cursor_position}
+CMP #$00
+BNE set_second_option
+
+// Setting first option
+LDA #$00
+STA {current_cursor_position}
+LDA {current_sg_to_practice}
+CMP {14_simple_or_advanced}
+BNE set_14_top_single_or_double_options_top
+
+// setting 14_simple_or_advanced options top
+LDA {14_simple_index}
+STA {current_sg_to_practice}
+LDA #$01
+STA {already_selected_sg_from_top_menu}
+JMP {done_offset}
+
+set_14_top_single_or_double_options_top:
+LDA {14_top_single_index}
+STA {current_sg_to_practice}
+LDA #$01
+STA {already_selected_sg_from_top_menu}
+JMP {done_offset}
+
+
+
+set_second_option:
+LDA #$00
+STA {current_cursor_position}
+LDA {current_sg_to_practice}
+CMP {14_simple_or_advanced}
+BNE set_14_top_single_or_double_options_bottom
+// setting 14_simple_or_advanced options bottom
+LDA {14_advanced_index}
+STA {current_sg_to_practice}
+LDA #$01
+STA {already_selected_sg_from_top_menu}
+JMP {done_offset}
+
+set_14_top_single_or_double_options_bottom:
+LDA {14_top_double_index}
+STA {current_sg_to_practice}
+LDA #$01
+STA {already_selected_sg_from_top_menu}
+JMP {done_offset}
+
+
+
+
+
+
+
+
+
+on_mode_selection_menu:
 LDA {currinput_oneframe}
 AND #$C0
 CMP #$80
 BEQ set_mode
 CMP #$40
+BEQ set_mode
+CMP #$C0
 BEQ set_mode
 JMP {done_offset}
 
@@ -163,16 +327,20 @@ LDA #$0E
 STA {walk_forward_count}
 LDA #$02
 STA {walk_back_count}
-LDA #$03
+LDY {current_sg_to_practice}
+INY
+LDA {sg_tables},y
 STA {target_pixel_high_byte}
-LDA #$28
+INY
+LDA {sg_tables},y
 STA {target_pixel_low_byte}
+INY
+LDA {sg_tables},y
+STA {sg_phase_count_target}
 
 LDA #$01
 STA {sg_practice_active_flag}
 STA {sg_phase_counter}
-LDA #$05
-STA {sg_phase_count_target}
 
 LDA #$00
 STA {pause_flag}
@@ -183,45 +351,25 @@ LDA #$0C
 STA {walk_forward_count}
 LDA #$04
 STA {walk_back_count}
-LDA #$03
+LDY {current_sg_to_practice}
+INY
+LDA {sg_tables},y
 STA {target_pixel_high_byte}
-LDA #$28
+INY
+LDA {sg_tables},y
 STA {target_pixel_low_byte}
+INY
+INY
+LDA {sg_tables},y
+STA {sg_phase_count_target}
 
 LDA #$01
 STA {sg_practice_active_flag}
 STA {sg_phase_counter}
-LDA #$07
-STA {sg_phase_count_target}
 
 LDA #$00
 STA {pause_flag}
 JMP {done_offset}
-
-// check current stage somewhere, and store the corresponding scroll glitch in an area of memory.
-// the momend the scroll glitch variables get initialized, check for this and use different variables depending on the value of current_sg_to_practice
-// if equal to 0, go to a special case where you display that there's no sg to practice here.
-// check_stage:
-// LDA {currstage}
-// CMP #$0E
-// BNE +
-// LDA {currsubstage}
-// CMP #$01
-// BNE +
-// LDA #$01
-// STA {current_sg_to_practice}
-// JMP somewhere idk
-// +:
-// CMP #$0D
-// BNE +
-// LDA {currsubstage}
-// CMP #$00
-// BNE +
-// LDA #$02
-// STA {current_sg_to_practice}
-// +:
-// JMP somewhere idk
-
 
 
 
@@ -269,12 +417,28 @@ STA {PPU_DATA}
 
 // checking if sg_phase_counter is even or odd
 // if odd simon has to go right, if even simon has to go left
+CLV
+LDY {current_sg_to_practice}
+LDA {sg_tables}, y
+CMP #$01
+BEQ rightward_sg
+
+// leftward sg
+LDA {sg_phase_counter}
+AND #$01
+CMP #$01
+BEQ check_leftward_motion
+BVC check_rightward_motion
+
+rightward_sg:
 LDA {sg_phase_counter}
 AND #$01
 CMP #$01
 BEQ check_rightward_motion
+BVC check_leftward_motion
 
 // checking leftward motion
+check_leftward_motion:
 LDA {simon_substate}
 AND #$01
 CMP #$01
@@ -283,6 +447,7 @@ BNE not_moving_right
 // it might be possible to cheese the scroll glitch from the other direction, idk, maybe not
 // this case is for moving right immediately after a frame where you moved left
 // todo: I think I need to check the high byte first, it won't matter for 14 glitch but it will for expanding the code to other SG's
+// actually, maybe not
 SEC
 CLV
 LDA {simon_x_low_byte}
@@ -545,25 +710,55 @@ mark_success:
 INC {sg_phase_counter}
 LDA {sg_phase_counter}
 CMP {sg_phase_count_target}
-BNE set_new_target_pixel
-// if scroll glitch was successful:
-JMP {sg_success_offset}
+BEQ end_of_sg_on_block
 
+// set new target pixel
+CLV
+LDY {current_sg_to_practice}
+LDA {sg_tables}, y
+CMP #$01
+BEQ rightward_sg2
 
-
-set_new_target_pixel:
+// leftward sg2
+LDA {sg_phase_counter}
 AND #$01
 CMP #$01
-BEQ set_target_pixel_forward
+BEQ set_target_pixel_forward_on_leftward_sg
+BVC set_target_pixel_backward_on_leftward_sg
+
+rightward_sg2:
+LDA {sg_phase_counter}
+AND #$01
+CMP #$01
+BEQ set_target_pixel_forward_on_rightward_sg
+BVC set_target_pixel_backward_on_rightward_sg
+
+
+set_target_pixel_forward_on_leftward_sg:
+CLV
 SEC
 LDA {target_pixel_low_byte}
-SBC {walk_back_count}
+SBC {walk_forward_count}
 STA {target_pixel_low_byte}
 BVC go_to_done
 DEC {target_pixel_high_byte}
 JMP {done_offset}
 
-set_target_pixel_forward:
+set_target_pixel_backward_on_leftward_sg:
+CLV
+CLC
+LDA {target_pixel_low_byte}
+ADC {walk_back_count}
+STA {target_pixel_low_byte}
+BVC go_to_done
+INC {target_pixel_high_byte}
+JMP {done_offset}
+
+
+
+
+set_target_pixel_forward_on_rightward_sg:
+CLV
 CLC
 LDA {target_pixel_low_byte}
 ADC {walk_forward_count}
@@ -572,7 +767,61 @@ BVC go_to_done
 INC {target_pixel_high_byte}
 JMP {done_offset}
 
+set_target_pixel_backward_on_rightward_sg:
+CLV
+SEC
+LDA {target_pixel_low_byte}
+SBC {walk_back_count}
+STA {target_pixel_low_byte}
+BVC go_to_done
+DEC {target_pixel_high_byte}
+JMP {done_offset}
+
+
+
+
+
+
 go_to_done:
+JMP {done_offset}
+
+end_of_sg_on_block:
+// if sg glitch was successful on the first target block:
+LDY {current_sg_to_practice}
+INY
+INY
+INY
+INY
+INY
+INY
+INY
+LDA {walk_forward_count}
+CMP #$0E
+BEQ skip_this_increment
+INY
+skip_this_increment:
+LDA {sg_tables}, y
+CMP {sg_phase_count_target}
+BNE move_to_next_block
+JMP {sg_success_offset}
+
+move_to_next_block:
+// LDA {sg_tables}, y
+STA {sg_phase_count_target}
+
+LDY {current_sg_to_practice}
+INY
+INY
+INY
+INY
+INY
+
+
+LDA {sg_tables}, y
+STA {target_pixel_high_byte}
+INY
+LDA {sg_tables}, y
+STA {target_pixel_low_byte}
 JMP {done_offset}
 
 
@@ -638,6 +887,7 @@ JMP {failure_amount_offset}
 
 
 org {failure_amount_offset}
+
 LDA #$20
 STA {PPU_ADDR}
 LDA #$80
@@ -649,6 +899,10 @@ STA {PPU_DATA}
 LDA #$00
 STA {PPU_DATA}
 LDA {failure_pixel_count}
+CMP #$DA
+BCC draw_number_as_is
+LDA #$DD
+draw_number_as_is:
 STA {PPU_DATA}
 LDA #$E5
 STA {PPU_DATA}
@@ -672,7 +926,54 @@ RTS
 
 
 
+org {menu_tables}
+
+// FF
+// Y = 0x0
+db $D1,$D4,$F2,$EF,$EB // 14SPL
+db $D1,$D4,$E0,$E3,$F5 // 14ADV
+// FE
+// Y = 0xA,
+db $F3,$EF,$F2,$ED,$EB // TPSNL
+db $F3,$EF,$E3,$E1,$EB // TPDBL
+// Anything else
+// Y = 0x14
+db $D2,$DD,$D1,$D4,$00 // 2-14
+db $D4,$DD,$D1,$D2,$00 // 4-12
 
 
+
+org {sg_tables}
+
+// Tables for values of each scroll glitch
+// in order:
+// direction of sg (#$00 if leftward, #$01 if rightward)
+// intial traget high byte for first block
+// intial traget low byte for first block
+// phase counter in 2-14 mode for first block
+// phase counter in 4-12 mode for first block
+// intial traget high byte for second block
+// intial traget low byte for second block
+// total phase counter in 2-14 mode
+// total phase counter in 4-12 mode
+// if there's ever a scroll glitch found that requires moving three blocks, the table could be expanded
+
+// stage 14 simple
+db $01,$03,$28,$05,$07,$03,$28,$05,$07
+// stage 14 advanced
+db $01,$02,$C4,$07,$09,$03,$28,$0B,$0F
+// stage 14 top single
+db $00,$04,$5B,$07,$09,$04,$5B,$07,$09
+// stage 14 top double
+db $00,$04,$5B,$07,$09,$04,$3B,$0D,$11
+// stage 13
+db $01,$00,$A4,$07,$09,$00,$A4,$07,$09
+// stage 06 crushers
+db $00,$01,$D9,$07,$09,$01,$B9,$0D,$11
+// stage 17 placeholder
+db $00,$00,$00,$00,$00,$00,$00,$00,$00
+// stage 17 placeholder
+db $00,$00,$00,$00,$00,$00,$00,$00,$00
+// stage 17 placeholder
 
 

b79e378cb78443d3211b2cc38315d61f9add0b29
SBDWolf <90091268+SBDWolf@users.noreply.github.com>
2022-05-19 22:30:17 +0200

    Add files via upload

--- src/test.asm
+++ src/test.asm
@@ -6,15 +6,26 @@ STA $2003
 LDY #$02
 STY $4014
 
-// check for stage 13-1, if anywhere else don't run this code
+
+
+// check for stagse 13-1 and 6-1, if anywhere else don't run this code
+CLV
 LDA {currstage}
 CMP #$0D
+BNE check_for_stage_6
+LDA {currsubstage}
+BEQ stage_13_code
+check_for_stage_6:
+LDA {currstage}
+CMP #$06
 BNE done
 LDA {currsubstage}
-CMP #$00
-BNE done
+BEQ stage_6_code
+BVC done
 
 
+
+stage_13_code:
 LDA #$21
 STA {PPU_ADDR}
 LDA #$B1
@@ -30,6 +41,41 @@ BVC done
 setmaxhealth:
 LDA #$40
 STA {simon_health}
+BVC done
+
+
+stage_6_code:
+LDA #$22
+STA {PPU_ADDR}
+LDA #$39
+STA {PPU_ADDR}
+LDA {PPU_DATA}
+LDA {PPU_DATA}
+CMP #$69
+BNE check_second_block
+
+
+kill_simon:
+LDA #$00
+STA {simon_health}
+BVC done
+
+
+check_second_block:
+LDA #$22
+STA {PPU_ADDR}
+LDA #$35
+STA {PPU_ADDR}
+LDA {PPU_DATA}
+LDA {PPU_DATA}
+CMP #$30
+BNE setmaxhealth
+BVC kill_simon
+
+setmaxhealth:
+LDA #$40
+STA {simon_health}
+
 
 done:
 RTS
